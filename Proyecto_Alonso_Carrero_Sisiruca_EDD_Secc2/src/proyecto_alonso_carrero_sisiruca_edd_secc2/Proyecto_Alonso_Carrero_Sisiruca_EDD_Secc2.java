b79b4e592632ae785a66766c2f482a2a6697b0da
Angel Carrero <angel.carrero@correo.unimet.edu.ve>
2023-05-27 15:12:45 -0400

    prueba_comienzo

---
+++ Proyecto_Alonso_Carrero_Sisiruca_EDD_Secc2/src/proyecto_alonso_carrero_sisiruca_edd_secc2/Proyecto_Alonso_Carrero_Sisiruca_EDD_Secc2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto_alonso_carrero_sisiruca_edd_secc2;
+
+/**
+ *
+ * @author acarr
+ */
+public class Proyecto_Alonso_Carrero_Sisiruca_EDD_Secc2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
